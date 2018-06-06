a9afd36c6ffe2d25b45d024928bff661ac7c79ae
徐思远 <xusy@bzhcloud.com>
2018-06-06 16:47:37 +0800

    passport 安装集成

---
+++ src/server/auth.js
@@ -0,0 +1,10 @@
+const passport = require('koa-passport');
+const knex = require('./db/connection');
+
+passport.serializeUser((user, done) => { done(null, user.id); });
+
+passport.deserializeUser((id, done) => {
+  return knex('users').where({ id }).first()
+    .then((user) => { done(null, user); })
+    .catch((err) => { done(err, null); });
+});
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,26 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
+const passport = require('koa-passport');
 const indexRoutes = require('./routes/index');
 const movieRoutes = require('./routes/movies');
 
 const app = new Koa();
 const PORT = process.env.PORT || 3000;
 
+// sessions
+app.keys = ['super-secret-key'];
+app.use(session(app));
+
+// body parser
 app.use(bodyParser());
+
+// authentication
+require('./auth');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
 app.use(indexRoutes.routes());
 app.use(movieRoutes.routes());
 
