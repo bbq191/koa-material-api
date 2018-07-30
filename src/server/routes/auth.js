4e78b5e8529de26be4e8cc03545b20dc4a7b18cc
徐思远 <xusy@bzhcloud.com>
2018-07-30 16:45:39 +0800

    register 测试 （post）

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -10,4 +10,17 @@ router.get('/auth/register', async (ctx) => {
   ctx.body = fs.createReadStream('./src/server/views/register.html');
 });
 
+router.post('/auth/register', async (ctx) => {
+  const user = await queries.addUser(ctx.request.body);
+  return passport.authenticate('local', (err, user, info, status) => {
+    if (user) {
+      ctx.login(user);
+      ctx.redirect('/auth/status');
+    } else {
+      ctx.status = 400;
+      ctx.body = { status: 'error' };
+    }
+  })(ctx);
+});
+
 module.exports = router;
